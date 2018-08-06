9de09089884813772d0ac5aa574ffe295b0223dc
Alfred Holmes <holmesey55@gmail.com>
2018-08-06 13:41:26 +0100

    Visulization

---
+++ Data Aquisition/Companies House Migrations/visulization/graphics.py
@@ -0,0 +1,35 @@
+import pygame
+from pygame.locals import QUIT
+
+class Graphics:
+    def __init__(self, width=800, height=600):
+        self.width = width
+        self.height = height
+
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.bg = pygame.Surface((self.width, self.height))
+        self.bg.fill((0, 0, 0))
+
+        self.surf = pygame.Surface((self.width, self.height))
+        self.screen.blit(self.bg, (0, 0))
+
+    def clear(self):
+        self.surf = pygame.Surface((self.width, self.height))
+
+    def flip(self):
+        self.screen.blit(self.bg, (0, 0))
+        self.screen.blit(self.surf, (0, 0))
+        pygame.display.flip()
+
+    def draw_point(self, x, y, r):
+        pygame.draw.circle(self.surf, (255, 255, 255), (x, y), r)
+
+    def draw_line(self, x, y, x_, y_):
+        pygame.draw.aaline((255, 255, 255), (x, y), (x_, y_))
+    def should_quit(self):
+        for e in pygame.event.get():
+            return e.type == QUIT
+    def quit(self):
+        pygame.quit()
---
+++ Data Aquisition/Companies House Migrations/visulization/main.py
@@ -0,0 +1,34 @@
+import graphics, location
+
+#min, max long: -6.65721989,  1.64949596
+#min, max lat: 	49.92332077, 60.50495148
+
+
+def main():
+    g = graphics.Graphics(1000, 1000)
+
+    print('Loading LA data...')
+    lm = location.LocationManager('data/geographic/local_authority_locations.csv', 'data/migrations/la_companies_house_migration.csv')
+    print('done')
+
+    for id, la in lm.local_authorities.items():
+        x, y = ll_to_xy((la.long, la.lat))
+        for la_m, n in la.emmigration.items():
+            x_, y_ = ll_to_xy(la_m.long, la_m.lat)
+            g.draw_line(x, y, x_, y_)
+        g.draw_point(int(x), int(y), 2)
+
+    while not g.should_quit():
+        #g.draw_point(5, 5, 20)
+        g.flip()
+        pass
+
+    g.quit()
+
+def ll_to_xy(position):
+        x = (position[0] - (-6.65)) / ((1.649 - -6.657)) * 750 + 125
+        y = 1000 -(position[1] - (49.9233)) / (60.5049 - 49.9233) * 900 - 50
+        return (x, y)
+
+if __name__ == '__main__':
+    main()
