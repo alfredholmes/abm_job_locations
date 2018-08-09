4af55aaed004fa4ce796c4bd741dc6e5ab1f09a5
Alfred Holmes <holmesey55@gmail.com>
2018-08-09 10:41:47 +0100

    basic visulization of company migrations

--- Data Aquisition/Companies House Migrations/visulization/graphics.py
+++ Data Aquisition/Companies House Migrations/visulization/graphics.py
@@ -1,5 +1,5 @@
 import pygame
-from pygame.locals import QUIT, BLEND_ADD
+from pygame.locals import *
 
 class Graphics:
     def __init__(self, width=800, height=600):
@@ -10,31 +10,38 @@ class Graphics:
 
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.bg = pygame.Surface((self.width, self.height))
-        self.bg.fill((0, 0, 0))
-
+        self.bg.fill(pygame.Color('#0b3954'))
+        #self.bg.fill((0, 0, 0))
         self.surf = pygame.Surface((self.width, self.height))
-        self.screen.blit(self.bg, (0, 0))
+        self.surf.blit(self.bg, (0, 0))
 
         self.temp_surf = pygame.Surface((self.width, self.height))
+        #self.surf.set_colorkey((0, 0, 0))
+        self.flip()
+
 
     def clear(self):
         self.surf = pygame.Surface((self.width, self.height))
 
     def flip(self):
-        self.screen.blit(self.bg, (0, 0))
         self.screen.blit(self.surf, (0, 0))
         pygame.display.flip()
 
     def draw_point(self, x, y, r):
         pygame.draw.circle(self.surf, (255, 255, 255), (x, y), r)
 
-    def draw_line(self, x, y, x_, y_):
-        pygame.draw.aaline(self.temp_surf, (255, 255, 255), (x, y), (x_, y_))
+    def draw_line(self, x, y, x_, y_, c, alpha):
+        temp = pygame.Surface((self.width, self.height))
+        temp.blit(self.bg, (0, 0))
+        pygame.draw.aaline(temp, pygame.Color(c), (x, y), (x_, y_))
+        temp.set_colorkey(pygame.Color('#0b3954'))
+        temp.set_alpha(alpha)
+        self.surf.blit(temp, (0, 0))
+        #self.surf.set_alpha(255)
 
-    def draw_temp_surf_with_alpha(self, alpha):
-        self.temp_surf.set_alpha(alpha)
-        self.surf.blit(self.temp_surf, (0, 0), special_flags=BLEND_ADD)
-        self.temp_surf = pygame.Surface((self.width, self.height))
+
+    def save_to_file(self, file):
+        pygame.image.save(self.surf, file)
 
     def should_quit(self):
         for e in pygame.event.get():
--- Data Aquisition/Companies House Migrations/visulization/main.py
+++ Data Aquisition/Companies House Migrations/visulization/main.py
@@ -1,11 +1,12 @@
-import graphics, location
+import graphics, location, time
 
 #min, max long: -6.65721989,  1.64949596
 #min, max lat: 	49.92332077, 60.50495148
 
 
 def main():
-    g = graphics.Graphics(1000, 1000)
+    size = 1000
+    g = graphics.Graphics(size, size)
 
     print('Loading LA data...')
     lm = location.LocationManager('data/geographic/local_authority_locations.csv', 'data/migrations/la_companies_house_migration.csv')
@@ -13,23 +14,28 @@ def main():
 
     for id, la in lm.local_authorities.items():
         print(id)
-        x, y = ll_to_xy((la.long, la.lat))
-        g.draw_point(int(x), int(y), 2)
+        x, y = ll_to_xy((la.long, la.lat), size)
+        #g.draw_point(int(x), int(y), 1)
         for la_m, n in la.emmigration.items():
-            x_, y_ = ll_to_xy((la_m.long, la_m.lat))
-            g.draw_line(x, y, x_, y_)
-        g.draw_temp_surf_with_alpha(30)
+            x_, y_ = ll_to_xy((la_m.long, la_m.lat), size)
+            c = '#ff5a5f'
+            if y_ < y:
+                c = '#bfd7ea'
+            #g.flip()
+            g.draw_line(x, y, x_, y_, c, int(n / 300 * 255))
+
+
+    g.save_to_file('output.png')
+    g.flip()
 
     while not g.should_quit():
-        #g.draw_point(5, 5, 20)
-        g.flip()
         pass
 
     g.quit()
 
-def ll_to_xy(position):
-        x = (position[0] - (-6.65)) / ((1.649 - -6.657)) * 750 + 125
-        y = 1000 -(position[1] - (49.9233)) / (60.5049 - 49.9233) * 900 - 50
+def ll_to_xy(position, size):
+        x = (position[0] - (-6.65)) / ((1.649 - -6.657)) * 0.75 * size + 0.125 * size
+        y = size -(position[1] - (49.9233)) / (60.5049 - 49.9233) * size - 0.05 * size
         return (x, y)
 
 if __name__ == '__main__':
