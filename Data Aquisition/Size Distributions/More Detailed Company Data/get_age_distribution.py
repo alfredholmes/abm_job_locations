5b8846b1132a7b8e334df4da43d7e30da902102b
Alfred Holmes <holmesey55@gmail.com>
2018-08-10 23:22:08 +0100

    Age data aggregate stats

--- Data Aquisition/Size Distributions/More Detailed Company Data/get_age_distribution.py
+++ Data Aquisition/Size Distributions/More Detailed Company Data/get_age_distribution.py
@@ -1,18 +1,34 @@
 import csv
 
 FILES = ['2017/company_data_' + str(i) + '.csv' for i in range(10)]
-AGE_BANDS = []
+AGE_BANDS = [2, 4, 10]
 
 
 def main():
     companies = load_data()
+    las = {}
+    #first populate las dict
+    for c in companies:
+        if c[1] not in las:
+            las[c[1]] = [0] * (len(AGE_BANDS) + 1)
+
+    for c in companies:
+        for i, age in enumerate(AGE_BANDS):
+            if int(c[2]) / 12 < age:
+                las[c[1]][i] += 1
+                break
+        else:
+            las[c[1]][-1] += 1
 
 
-
-    for company in companies:
-
+    write_data(las)
 
 
+def write_data(data):
+    with open('age_data.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        for la, d in data.items():
+            writer.writerow([la] + d)
 
 def load_data():
     data = []
