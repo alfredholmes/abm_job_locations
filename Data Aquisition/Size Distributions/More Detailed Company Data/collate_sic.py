497717683d835ea87521b415ffd5b27f581a9171
Alfred Holmes <holmesey55@gmail.com>
2018-08-12 11:27:26 +0100

    Sic code analysis

--- Data Aquisition/Size Distributions/More Detailed Company Data/collate_sic.py
+++ Data Aquisition/Size Distributions/More Detailed Company Data/collate_sic.py
@@ -1,26 +1,42 @@
 import csv
 
 def main():
-    ons = read('ons_2013_sic_total.csv')
-    ch  = read('sic_ch_2013_total.csv')
+    ons = read_ons('ons_2013_sic_total.csv')
+    ch  = read_ch('sic_ch_2013_total.csv')
 
+    data = {}
+    for c,n in ch.items():
+        if int(str(c)[:2]) in data:
+            data[int(str(c)[:2])][0] += n
+        elif int(str(c)[:2]) in ons:
+            data[int(str(c)[:2])] = [n, ons[int(str(c)[:2])]]
+        else:
+            print(int(str(c)[:2]))
     with open('2013_sic_comparison.csv', 'w') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(['sic', 'ch', 'ons'])
-        for sic, total in ch.items():
-            if sic not in ons:
-                print(sic)
-            else:
-                writer.writerow([sic, total, ons[sic]])
+        for sic, d in data.items():
+            writer.writerow([sic] + d)
 
 
-def read(file):
+
+
+def read_ons(file):
+    data = {}
+    with open(file, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            if len(line[0]) == 2:
+                data[int(line[0])] = int(line[1])
+    return data
+
+
+def read_ch(file):
     data = {}
     with open(file, 'r') as csvfile:
         reader = csv.reader(csvfile)
         for line in reader:
             try:
-                data[int(line[0][:4])] = line[1]
+                data[int(line[0])] = int(line[1])
             except:
                 pass
     return data
