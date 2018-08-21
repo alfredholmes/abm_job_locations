b180ff713a0bfb61601eadf9261d572b67b7f6c6
Alfred Holmes <holmesey55@gmail.com>
2018-08-21 11:10:31 +0100

    Prediction of local authority size distributions using SIC

--- Data Analysis/SIC Code to Estimate Size Distributions/evaluate_distributions.py
+++ Data Analysis/SIC Code to Estimate Size Distributions/evaluate_distributions.py
@@ -6,7 +6,6 @@ import csv
 SIZES = ['0-4','5-9','10-19','20-49', '50-99','100-249','250+']
 BANDS = [0, 5, 10, 20, 50, 100, 250, np.inf]
 
-BROAD_GROUPS = [(0, 4), (5, 40), (45, 46), (47, 48), (49, 54), (55, 57), (58, 64), (64, 67), (69, 76), (77, 83), (84, 85), (85, 86), (86, 89), (90, 100)]
 
 def main():
     params = get_ln_params()
@@ -35,7 +34,7 @@ def main():
         plt.scatter(d['x'], d['y'], label=s)
 
     plt.plot([0, 1], [0, 1], color='black', label='Target')
-    plt.xaxis
+
     plt.legend()
     plt.savefig('sic_dist_replication.png')
     plt.figure(1)
@@ -65,32 +64,7 @@ def get_sic_dists():
                 pass
     return data, totals
 
-def get_broad_industry_data():
-    data = {}
-    with open('2017_SIC_Size_Distributions.csv', 'r') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for line in reader:
-            try:
-                data[int(line['SIC'][:2])] = np.array([int(line[s]) for s in SIZES])
-            except:
-                pass
-        r = {}
-        for group in BROAD_GROUPS:
-            totals = np.zeros(len(SIZES))
-            for i in range(group[0], group[1]):
-                if i in data:
-                    totals += data[i]
-            r[group] = totals
 
-    return r
-
-def get_broad_industry_params():
-    data = {}
-    with open('sic_broad_group_params.csv', 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        for line in reader:
-            data[line[0]] = {'mean': float(line[1]),  'sd': float(line[2])}
-    return data
 
 def get_ln_params():
     data = {}
