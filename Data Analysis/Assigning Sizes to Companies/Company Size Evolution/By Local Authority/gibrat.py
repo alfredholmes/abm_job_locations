Fixed calculating mean and variance of rv to determine company growth

Data Analysis/Assigning Sizes to Companies/Company Size Evolution/By Local Authority/gibrat.py
@@ -5,8 +5,6 @@ from scipy.optimize import minimize, Bounds, bisect, newton_krylov, anderson
 import numpy as np
 import csv, datetime as dt
 
-TARGET = [0.24247032943908334, 1.732968915296001]
-
 
 def main():
     las = get_ages_by_la()
@@ -20,8 +18,7 @@ def main():
             continue
 
         target_mean = lognorm.mean(la_params[la]['sd'], scale=np.exp(la_params[la]['mean']))
-        target_variance = lognorm.var(TARGET[1], scale=np.exp(TARGET[0]))
-
+        
 
         mu = bisect(lambda x: mean(x, ages, target_mean), 0, 0.03)
         la_growth_params[la] = mu

Data Analysis/Assigning Sizes to Companies/Growth Independent of Companies Size/simulate_by_la.py
@@ -28,15 +28,19 @@ def main():
             if age == 0:
                 continue
             try:
-                total += np.sum(lognorm.rvs(sd * age, loc=np.exp(mean * age), size=n))
+                total += n * (1 + mean) ** age
             except:
-                print(mean * age, mean, age)
-        if total_employment[la] < 3 * total and total < 3 * total_employment[la]:
-            results['x'].append(total_employment[la])
-            results['y'].append(total)
+                pass
+                #print(mean * age, mean, age)
+
+        results['x'].append(total_employment[la])
+        results['y'].append(total)
 
     plt.scatter(results['x'], results['y'])
+    #plt.plot(results['x'], results['x'])
+    plt.savefig('LA_predicted_employment_using_la_fitted_params.png')
     plt.show()
+
 
 def get_total_employment():
     las = {}
@@ -48,10 +52,10 @@ def get_total_employment():
 
 def get_la_params():
     las = {}
-    with open('../parameters_by_local_authority.csv', 'r') as csvfile:
+    with open('../la_growth_means.csv', 'r') as csvfile:
         reader = csv.reader(csvfile)
         for line in reader:
-            las[line[0]] = {'mean': float(line[1]), 'sd': float(line[2])}
+            las[line[0]] = {'mean': float(line[1]), 'sd': float(0)}
     return las
 
 def get_ages_by_la():